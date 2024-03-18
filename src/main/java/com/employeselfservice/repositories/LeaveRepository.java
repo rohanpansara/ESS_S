9dd2196d5ac77cca31c793e8f54b2ecd77c39af7
rohanpansara <120650660+rohanpansara@users.noreply.github.com>
2024-03-18 17:16:59 +0530

    Add files via upload

--- src/main/java/com/employeselfservice/repositories/LeaveRepository.java
+++ src/main/java/com/employeselfservice/repositories/LeaveRepository.java
@@ -4,7 +4,12 @@ import com.employeselfservice.models.Leave;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface LeaveRepository extends JpaRepository<Leave,Long> {
+import java.util.List;
 
+@Repository
+public interface LeaveRepository extends JpaRepository<Leave, Long> {
+    List<Leave> findByEmployee_IdAndStatus(Long employeeId, Leave.LeaveStatus status);
+
+    List<Leave> findByEmployeeId(Long employeeId);
 }
+
