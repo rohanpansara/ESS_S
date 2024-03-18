03bcbbedde384fa3e2f50912613ea300f2aa394f
rohanpansara <120650660+rohanpansara@users.noreply.github.com>
2024-03-18 17:16:19 +0530

    Add files via upload

--- src/main/java/com/employeselfservice/services/LeaveService.java
+++ src/main/java/com/employeselfservice/services/LeaveService.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Service
 public class LeaveService {
@@ -15,14 +16,35 @@ public class LeaveService {
     @Autowired
     private LeaveRepository leaveRepository;
 
+    public List<Leave> findAllLeavesForEmployee(Long employeeId) {
+        return leaveRepository.findByEmployeeId(employeeId);
+    }
+
+    public List<Leave> findAllApprovedLeavesForEmployee(Long employeeId) {
+        return leaveRepository.findByEmployee_IdAndStatus(employeeId, Leave.LeaveStatus.APPROVED);
+    }
+
     public String applyForLeave(LeaveRequest leaveRequest){
         Leave leave = new Leave();
         leave.setEmployee(new Employee(leaveRequest.getEmployeeId()));
         leave.setReason(leaveRequest.getLeaveReason());
         leave.setFrom(leaveRequest.getLeaveFrom());
         leave.setTo(leaveRequest.getLeaveTo());
-        leave.setType(leaveRequest.getLeaveType());
-        leave.setStatus(leaveRequest.getLeaveStatus());
+
+        if(findAllApprovedLeavesForEmployee(leaveRequest.getEmployeeId()).size()>18){
+            leave.setType(Leave.LeaveType.UNPAID);
+        }
+        else {
+            leave.setType(Leave.LeaveType.PAID);
+        }
+
+        if(leaveRequest.getLeaveStatus()==null) {
+            leave.setStatus(Leave.LeaveStatus.PENDING);
+        }
+        else {
+            leave.setStatus(leaveRequest.getLeaveStatus());
+        }
+
         leave.setAppliedOn(LocalDate.now());
 
         leaveRepository.save(leave);
